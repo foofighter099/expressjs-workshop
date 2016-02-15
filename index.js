5d27e4ccafbac06bbededd803f5e76a096d8c31c
foofighter0991 <andrewtognarini@hotmail.com>
2016-02-15 19:03:47 +0000

    exercise 2

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function (req, res) {
-  res.send('Hello World!');
+function hello(str){
+  return str;
+}
+
+app.get('/hello/:firstName', function (req, res) {
+  var theNameEntered = req.params.firstName;
+  var result = hello(theNameEntered);
+  res.send('Hello ' + result);
 });
 
 
 
-
+/*Create a web server that can listen to requests for /hello/:firstName, and respond with some HTML that says <h1>Hello _name_!</h1>. 
+For example, if a client requests /hello/John, the server should respond with <h1>Hello John!</h1>*/
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
